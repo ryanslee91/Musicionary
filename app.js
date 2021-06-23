6e209b36fe1405e629ecddbbff57c6a83357e511
Sang Hyun Lee <you@example.com>
2021-06-23 16:40:50 -0400

    wednesday p2

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const DOMAIN = '';
-const API_KEY = '';
-const BASE_URL = `${}?apikey=${}&`
+const DOMAIN = 'http://www.theaudiodb.com/api/v1/json/1/search.php?s=';
+
+const BASE_URL = `${DOMAIN}`;
+
+
+const button = document.querySelector('#searchC');
+console.log(button);
+
+const ip = document.querySelector('input')
+
+const search = () => {
+  console.log(ip.value);
+
+}
+
+button.addEventListener('click', search);
+
+//api
+//list results from api server
+//refresh page after new search
+//random generator
+//ad section with time interval
