65febe56f8676fe955a800eec1b501802553b5ed
Sang Hyun Lee <you@example.com>
2021-06-23 19:52:39 -0400

    wed3

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-const DOMAIN = 'http://www.theaudiodb.com/api/v1/json/1/search.php?s=';
+// function fetchData(artist) {
+//   const url = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
+//   axios.get(url)
+//     .then((response) => {
+//       console.log(response)
+//     })
+//     .catch((error) => {
+//       console.error(error)
+//     })
+// }
+fetchData('Coldplay')
 
-const BASE_URL = `${DOMAIN}`;
+async function fetchData(artist) {
+  const url = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
+  try {
+    const response = await axios(url)
+    // console.log(response.data.artists[0])
+    showArtInfo(response.data.artists[0])
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function showArtInfo(data) {
+  const dataContainer = document.querySelector('#artist-data')
+  console.log("Inside show data", data.strArtist)
+  //strArtist
+  const strArtist = document.createElement('h1')
+  strArtist.textContent = data.strArtist
+  dataContainer.append(strArtist)
+  //strAristLogo
+  //strGenre
+  //strLabel
+  //strFacebook
+  //   //strAristClearart
+}
 
 
 const button = document.querySelector('#searchC');
