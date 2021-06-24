ffb639b9ca45bb8cb893e48cc2df903accca662d
Sang Hyun Lee <you@example.com>
2021-06-23 22:28:06 -0400

    wed4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //       console.error(error)
 //     })
 // }
-fetchData('Coldplay')
+// fetchData('linkin_park')
 
 async function fetchData(artist) {
   const url = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
@@ -23,33 +23,37 @@ async function fetchData(artist) {
 
 function showArtInfo(data) {
   const dataContainer = document.querySelector('#artist-data')
-  console.log("Inside show data", data.strArtist)
-  //strArtist
-  const strArtist = document.createElement('h1')
-  strArtist.textContent = data.strArtist
-  dataContainer.append(strArtist)
-  //strAristLogo
-  //strGenre
-  //strLabel
-  //strFacebook
-  //   //strAristClearart
+  let artInfo = `
+    <h1 style="font-family: helvetica; text-align: center">${data.strArtist}</h1>
+    <img src="${data.strArtistClearart}" style="height: 150px; width: 150px; display: block; margin-left: auto; margin-right:auto">
+    <h4 style="font-family: helvetica; text-align: center">GENRE: ${data.strGenre}</h4>
+    <h4 style="font-family: helvetica; text-align: center">LABEL: ${data.strLabel}</h4>
+    <h4 style="font-family: helvetica; text-align: center">WEB: ${data.strFacebook}</h4>
+    `
+  // //how to make facebook website a href? 
+  dataContainer.insertAdjacentHTML('beforeend', artInfo)
+  return artInfo
 }
 
+const form = document.querySelector('#artist-form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const inputValue = document.querySelector('#artist-search').value
+  // console.log(inputValue)
 
-const button = document.querySelector('#searchC');
-console.log(button);
+  //   alert("Your artist is not found!")
+  // how to alert when there is typo, which would bring no data?
+  removeArtist()
+  fetchData(inputValue)
+  document.querySelector('#artist-search').value = ''
+})
 
-const ip = document.querySelector('input')
-
-const search = () => {
-  console.log(ip.value);
-
+function removeArtist() {
+  const dataContainer = document.querySelector('#artist-data')
+  while (dataContainer.lastChild) {
+    dataContainer.removeChild(dataContainer.lastChild)
+  }
 }
 
-button.addEventListener('click', search);
-
-//api
-//list results from api server
-//refresh page after new search
 //random generator
 //ad section with time interval
